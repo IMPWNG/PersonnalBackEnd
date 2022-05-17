91607b70645f6e9d46e0331dff0e08ad768983ed
IMPWNG <matisse.ach+github.com>
2022-05-17 12:34:21 -0400

    ModalAuth

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -1,7 +1,94 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
+
+import SignIn from "./components/Auth/SignIn";
+
 export default function App () {
+  const [showModal, setShowModal] = useState(false);	
+
   return (
-    <div className="text-red-500">
-      <h1>Hello World</h1>
-    </div>
-  )
+    <>
+    <button 
+      className="rounded-full border-2 border-gray-600 p-2"	
+      type="button"
+      onClick={() => setShowModal(true)}>
+      Sign In
+    </button>
+    {showModal ? (
+      <div className="fixed inset-0 z-50 overflow-hidden">
+        <div className="flex items-center justify-center min-h-screen bg-gray-900">
+          <div className="relative w-full max-w-xs">
+            <div className="bg-white rounded-lg px-6 pt-16 pb-20 mb-4">
+    )}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+    //     <Router>
+    //       <div>
+    //         Hello
+
+    //           <button
+    //             className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    //             type="button"
+    //             data-modal-toggle="authentication-modal"
+    //           >
+    //             test
+    //           </button>
+    //           <div id="authentication-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+    //     <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+
+    //         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+    //             <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="authentication-modal">
+    //                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+    //             </button>
+    //             <div className="py-6 px-6 lg:px-8">
+    //                 <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+    //                 <form className="space-y-6" action="#">
+    //                     <div>
+    //                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+    //                         <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+    //                     </div>
+    //                     <div>
+    //                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+    //                         <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+    //                     </div>
+    //                     <div className="flex justify-between">
+    //                         <div className="flex items-start">
+    //                             <div className="flex items-center h-5">
+    //                                 <input id="remember" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+    //                             </div>
+    //                             <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+    //                         </div>
+    //                         <a href="#" className="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+    //                     </div>
+    //                     <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+    //                     <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+    //                         Not registered? <a href="#" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+    //                     </div>
+    //                 </form>
+    //             </div>
+    //         </div>
+    //     </div>
+    // </div>
+
+    //         <Link to={"/sign-up"}>
+    //           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    //             Sign up
+    //           </button>
+    //         </Link>
+    //       </div>
+    //     </Router>
+  );
 }

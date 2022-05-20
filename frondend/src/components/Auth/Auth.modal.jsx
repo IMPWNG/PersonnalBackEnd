15a62388fc1dfcf7738417af1d5b3e1936632eb6
IMPWNG <matisse.ach+github.com>
2022-05-20 16:54:41 -0400

    useNavigate routes issues + rework AuthModal

--- frondend/src/components/Auth/Auth.modal.jsx
+++ frondend/src/components/Auth/Auth.modal.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "@reach/router";
 
 import { signup, signin } from "../../actions/Auth.action";
-import { AUTH } from "../../constants/actionTypes";
 
-const initialState = { username: "", password: "", confirmPassword: "", email: "" };
- 
+
+const initialState = {
+  username: "",
+  password: "",
+  confirmPassword: "",
+  email: "",
+};
+
 export default function SignUp() {
   const [form, setFrom] = useState(initialState);
   const [isSignup, setIsSignup] = useState(false);
@@ -16,7 +21,7 @@ export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const handleShowPassword = () => setShowPassword(!showPassword);
 
-  const swithMode = () => {
+  const switchMode = () => {
     setFrom(initialState);
     setIsSignup((prevIsSignup) => !prevIsSignup);
     setShowPassword(false);
@@ -35,17 +40,66 @@ export default function SignUp() {
   const handleChange = (e) => {
     setFrom({ ...form, [e.target.name]: e.target.value });
   };
- 
+
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Create an Account
+            {isSignup ? "Sign up" : "Sign in"}
           </h2>
         </div>
-        <form className="mt-8 space-y-6" onSubmit={SignUp}>
-          
+        <form className="mt-8 space-y-6 flex flex-col" onSubmit={handleSubmit}>
+          {isSignup && (
+            <>
+              <input
+                handleChange={handleChange}
+                type="text"
+                name="username"
+                label="Username"
+                className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+              />
+            </>
+          )}
+          <input
+            handleChange={handleChange}
+            type="email"
+            name="email"
+            label="Email"
+            className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+          />
+          <input
+            handleChange={handleChange}
+            type={showPassword ? "text" : "password"}
+            handleShowPassword={handleShowPassword}
+            name="password"
+            label="Password"
+            className="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+          />
+          {isSignup && (
+            <input
+              name="ConfirmPassword"
+              label="Repeat Password"
+              handleChange={handleChange}
+              type="Password"
+            />
+          )}
+          <button
+            type="submit"
+            className="mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent text-white focus:outline-none bg-blue-500 text-blue-100 hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer inline-flex items-center w-full justify-center items-center font-medium focus:outline-none"
+          >
+            { isSignup ? "Sign up" : "Sign in" }
+          </button>
+          <div className="flex flex-col items-center mt-5">
+              <button 
+                onClick={switchMode}
+                className="mb-">
+                  {isSignup
+                    ? "Already have an account? Sign in"
+                    : "Don't have an account? Sign Up"
+                  }
+              </button>
+          </div>
         </form>
       </div>
     </div>
